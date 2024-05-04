d0cc5d639c95f85ce325f8587f2df06eac3e33c8
om-abhiwan <om.abhiwan@gmail.com>
2024-05-04 17:45:50 +0530

    Code Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-
 import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios';
 
 function App() {
 
-const [longitude,setLongtitude] = useState("")
-const [latitude,setLatitude] = useState("")
+  const [longitude, setLongtitude] = useState("")
+  const [latitude, setLatitude] = useState("")
 
-  console.log(location)
+
+  // setting category
+  const [category,setCategory] = useState("")
+
+
+  // console.log(location)
 
   useEffect(() => {
     const watchId = navigator.geolocation.watchPosition((position) => {
@@ -21,6 +26,51 @@ const [latitude,setLatitude] = useState("")
   }, []);
 
 
+  const handleSendInfo = async() =>{
+    const resp =await axios.post("https://google-map-backend-7oji.onrender.com/getinfo",{
+      long:longitude,
+      lati:latitude
+    })
+    setCategory(resp.data.category)
+    console.log(resp.data.category)
+  }
+
+  useEffect(()=>{
+    handleSendInfo()
+  },[longitude,latitude])
+
+
+
+
+  // implementing new api
+
+  // const CLIENT_ID = 'GO4WHBFZIGBTFHKR4DXHHWQPXISNPW3XCCWJVZWJXYG3ZSSU';
+  // const CLIENT_SECRET = '0EHGSLY03AM0BMQ21QWWUPWPJPEJZ2SS1OOO11SBJHGRADSE';
+  // const API_URL = 'https://api.foursquare.com/v2/venues/search';
+
+
+  // // Example function to search for recommended venues near a location
+  // async function searchVenues(latitude, longitude) {
+  //   const today = new Date();
+  //   const date = today.toISOString().slice(0, 10).replace(/-/g, ''); // Format: YYYYMMDD
+
+  //   const url = `${API_URL}?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&v=${date}&ll=${latitude},${longitude}`;
+
+  //   try {
+  //     const response = await fetch(url);
+  //     console.log(response)
+  //     const data = await response.json();
+  //     // Handle the data, e.g., display recommended venues
+  //     console.log(data.response.groups[0].items);
+  //   } catch (error) {
+  //     console.error('Error fetching data:', error);
+  //   }
+  // }
+
+  // searchVenues(latitude, longitude)
+
+
+
   return (
     <>
       <p>
@@ -28,6 +78,11 @@ const [latitude,setLatitude] = useState("")
         <br />
         Latitude : {latitude}
       </p>
+
+      Category : {category}
+
+
+
     </>
 
   )
